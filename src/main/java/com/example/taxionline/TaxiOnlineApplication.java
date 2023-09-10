f7f6c4a89bdfa494eb3cb5a20cb2ba13de25c0cb
Mohsen <imyazdani@gmail.com>
2023-09-10 21:23:25 +0330

    Initial spring boot

---
+++ src/main/java/com/example/taxionline/TaxiOnlineApplication.java
@@ -0,0 +1,13 @@
+package com.example.taxionline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaxiOnlineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaxiOnlineApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/taxionline/TaxiOnlineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.taxionline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaxiOnlineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
