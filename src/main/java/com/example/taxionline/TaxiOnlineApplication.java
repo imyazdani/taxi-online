423854ee2a74acfc0507c1b75477b3d8f8be8535
Mohsen <imyazdani@gmail.com>
2023-09-13 10:47:20 +0330

    fix a bug: convert the AppPropertiesConfig to be an immutable config class

--- src/main/java/com/example/taxionline/TaxiOnlineApplication.java
+++ src/main/java/com/example/taxionline/TaxiOnlineApplication.java
@@ -1,9 +1,12 @@
 package com.example.taxionline;
 
+import com.example.taxionline.config.AppPropertiesConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(AppPropertiesConfig.class)
 public class TaxiOnlineApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/example/taxionline/config/AppPropertiesConfig.java
+++ src/main/java/com/example/taxionline/config/AppPropertiesConfig.java
@@ -1,26 +1,44 @@
 package com.example.taxionline.config;
 
-import lombok.Data;
+import lombok.Getter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.stereotype.Component;
+import org.springframework.boot.context.properties.ConstructorBinding;
 
-@Data
-@Component
-@ConfigurationProperties("app")
+
+@Getter
+@ConfigurationProperties(prefix = "app")
+@ConstructorBinding
 public class AppPropertiesConfig {
 
     private final Security security;
     private final Trip trip;
 
-    @Data
+    public AppPropertiesConfig(Security security, Trip trip) {
+        this.security = security;
+        this.trip = trip;
+    }
+
+    @Getter
+    @ConstructorBinding
     public static class Security{
         private final String passengerRegister;
         private final String driverRegister;
+
+        public Security(String passengerRegister, String driverRegister) {
+            this.passengerRegister = passengerRegister;
+            this.driverRegister = driverRegister;
+        }
     }
 
-    @Data
+    @Getter
+    @ConstructorBinding
     public static class Trip{
         private final Integer distance;
         private final Integer limit;
+
+        public Trip(Integer distance, Integer limit) {
+            this.distance = distance;
+            this.limit = limit;
+        }
     }
 }
